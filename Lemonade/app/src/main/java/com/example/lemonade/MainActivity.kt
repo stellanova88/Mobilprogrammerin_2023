8780266090dbd2e9f7365fdf9b47609cde4f6851
Børre Stellander <64796748+stellanova88@users.noreply.github.com>
2023-08-23 12:53:46 +0200

    added required functionality

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +35,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     LemonadePreview()
                 }
@@ -34,16 +43,66 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
-fun Lemonade(modifier: Modifier = Modifier){
+fun Lemonade( modifier: Modifier = Modifier){
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0)
+    }
+    when (currentStep) {
+        1 -> LemonTextAndImage(
+            imageName = R.drawable.lemon_tree,
+            imageDescriptionText = R.string.Lemon_tree,
+            imageText = R.string.lemon_tree_text,
+            goNextImage = {currentStep = 2
+                squeezeCount = (2..4).random()
+            })
+        2 -> LemonTextAndImage(
+            imageName = R.drawable.lemon_squeeze,
+            imageDescriptionText = R.string.Lemon,
+            imageText = R.string.lemon_squeeze_text,
+            goNextImage = {
+                squeezeCount --
+                if (squeezeCount == 0) {
+                    currentStep = 3
+                }
+            })
+        3 -> LemonTextAndImage(
+            imageName = R.drawable.lemon_drink,
+            imageDescriptionText = R.string.Glass_of_lemonade,
+            imageText = R.string.lemon_drink_text,
+            goNextImage = {currentStep = 4})
+        4 -> LemonTextAndImage(
+            imageName = R.drawable.lemon_restart,
+            imageDescriptionText = R.string.Empty_glass,
+            imageText = R.string.lemon_restart_text,
+            goNextImage = {currentStep = 1})
+    }
+}
+
+@Composable
+fun LemonTextAndImage(imageName: Int,
+                      imageDescriptionText: Int,
+                      imageText: Int,
+                      goNextImage: () -> Unit,
+                      modifier: Modifier = Modifier){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier,
-    ) {
-        Image(painterResource(R.drawable.lemon_tree),
-            contentDescription = stringResource(R.string.Lemon_tree))
+        ) {
+        Button(
+            onClick = goNextImage,
+            shape = RoundedCornerShape(35.dp)
+            ) {
+            Image(
+                painterResource(imageName),
+                contentDescription = stringResource(imageDescriptionText),
+            )
+        }
+
         Text(
-            text = stringResource(R.string.lemon_tree_text))
+            text = stringResource(imageText)
+        )
     }
 }
 
